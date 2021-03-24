6796baedea8921bc245616d3ed4059c5375a4bad
Jack Hunt <jackmileshunt@gmail.com>
2021-03-24 22:18:22 +0000

    Clean script.

--- clean.sh
+++ clean.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-fnames[0]='.git'
-fnames+=($(git ls-files))
-printf "FNAME: %s\n" "${fnames[@]}"
+fnames_keep[0]='.git'
+fnames_keep+=($(git ls-files))
+
+fnames_all=($(ls -a))
+
+for f in "${fnames_all[@]}"
+do
+    if [[ ! " ${fnames_keep[@]} " =~ " ${f} " ]]; then
+        echo "Deleting: $f"
+        rm -rf "$f"
+    fi
+done
