457ad8ffed32ebc6ffbba80a46bd8d743c26637f
Jack Hunt <JackHunt@users.noreply.github.com>
2023-06-13 23:57:31 +0100

    Start fresh config.

--- init.el
+++ init.el
@@ -1,5 +1,108 @@
-(package-initialize)
+;; Set some sane UI defaults.
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+(menu-bar-mode -1)
+(setq visible-bell t)
 
-;; Load Emacs Config from Org file.
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
-(garbage-collect)
+;; Set font - TODO: set size.
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 280)
+
+;; Setup use-package
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Theme - TODO: get from xresources.
+(use-package doom-themes
+  :init (load-theme 'doom-gruvbox))
+
+;; Colour coded parenthesis etc.
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Line & column numbers. Disabled for org & term.
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+(dolist (mode '(org-mode-hook
+                term-mode-hook))
+    shell-mode-hook
+                eshell-mode-hook
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+ 
+
+;; Setup nice modeline - TODO: customize height
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+;; Keep command history.
+(use-package command-log-mode)
+
+;; Setup Ivy.
+(use-package ivy
+  :diminish
+;;  :bind (("C-s" . swiper)
+;;         :map ivy-minibuffer-map
+;;         ("TAB" . ivy-alt-done)	
+;;         ("C-l" . ivy-alt-done)
+;;         ("C-j" . ivy-next-line)
+;;         ("C-k" . ivy-previous-line)
+;;         :map ivy-switch-buffer-map
+;;         ("C-k" . ivy-previous-line)
+;;         ("C-l" . ivy-done)
+;;         ("C-d" . ivy-switch-buffer-kill)
+;;         :map ivy-reverse-i-search-map
+;;         ("C-k" . ivy-previous-line)
+;;         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+;; Command descriptions & key combos in ivy.
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1)) 
+
+;; Setup counsel.
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))) 
+ 
+;; Shows possible key bindings given a partial sequence. 
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
+
+;; Better docs for C-h v etc
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+  
+  
---
+++ old/init.el
@@ -0,0 +1,5 @@
+(package-initialize)
+
+;; Load Emacs Config from Org file.
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(garbage-collect)
