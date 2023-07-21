4d1a4591cbf6b03de397c8c4cf3c3c76f23bdbc7
Jack Hunt <jackmileshunt@gmail.com>
2023-07-21 17:23:51 +0100

    Minor changes

--- install_python_deps.sh
+++ install_python_deps.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-python3 -m pip install \
-  python-language-server[all] \
-  pyls-black \
-  pyls-isort \
-  pyls-mypy \
-  pyls-flake8 \
-  debugpy
+# python3 -m pip install \
+#   python-language-server[all] \
+#   pyls-black \
+#   pyls-isort \
+#   pyls-mypy \
+#   pyls-flake8 \
+#   debugpy
+
+sudo apt install python3-pyls-*
