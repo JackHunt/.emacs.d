da4d45cc4e40324b28d6bfd37ea454af9527d907
Jack Hunt <jackmileshunt@gmail.com>
2023-08-20 23:49:54 +0100

    Org roam

--- init.el
+++ init.el
@@ -444,6 +444,32 @@
 
     ))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ORG ROAM
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package emacsql
+  :ensure t)
+
+(use-package emacsql-sqlite
+  :ensure t)
+
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/GitHub/org-roam-notes/"))
+  (org-roam-db-location (file-truename "~/GitHub/org-roam-notes/"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+
+  (require 'org-roam-protocol))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
