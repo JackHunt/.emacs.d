7c870bf8e6372b17e348bb74a013ebbb7f2e7189
Jack Hunt <jackmileshunt@gmail.com>
2025-04-14 23:09:32 +0100

    Update init.el

--- init.el
+++ init.el
@@ -1,3 +1,22 @@
-(require 'org)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+
+(if (fboundp 'straight-use-package)
+    (straight-use-package 'org)
+  (require 'org))
+
 (org-babel-load-file
-  (expand-file-name "config.org" user-emacs-directory))
+ (expand-file-name "config.org" user-emacs-directory))
